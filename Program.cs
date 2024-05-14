3cd2953c42e2990c2259b82a6cbbc395c172d82f
Fernando Flores <fernandofloresfacet@gmail.com>
2024-05-14 15:59:38 -0300

    ejercicio1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a: "+a);
+Console.WriteLine("valor de b: "+b);
+int num;
+Console.WriteLine("ingrese numero");
+string buff=Console.ReadLine();
+if (int.TryParse(buff, out num))
+{
+    if(num>0)
+    {
+        int aux=num;
+        do
+        {
+            int mostrar=aux%10;
+            Console.Write(mostrar);
+            aux=aux/10;
+        } while (aux>0);
+
+    }else{
+        Console.WriteLine("no es un numero positivo");
+    }
+}
